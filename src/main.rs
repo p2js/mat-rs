b82d086bd5cdf967c56913e0e418512dc1fb6ee4
Alfio <tomarchioalfio5@gmail.com>
2023-10-31 17:11:45 +0000

    implement Display trait for Mat

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,6 @@ use std::ops::Deref;
 #[derive(Debug)]
 struct Mat<const R: usize, const C: usize>([[f64; C]; R]);
 
-
-
 impl<const R: usize, const C: usize> Deref for Mat<R,C> {
     type Target = [[f64; C]; R];
     fn deref(&self) -> &Self::Target {
@@ -14,6 +12,29 @@ impl<const R: usize, const C: usize> Deref for Mat<R,C> {
     }
 }
 
+impl<const R: usize, const C: usize> Display for Mat<R,C> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let max_column_lengths = self.transpose().map(|column| {
+            column.map(|x| x.to_string().len()).iter().copied().max().unwrap()
+        });
+
+        for (row_index, row) in self.iter().enumerate() {
+            let row_string = row.iter().enumerate()
+            .map(|(column_index, column)| {
+                format!("{:^len$}", column.to_string(), len = max_column_lengths[column_index])
+            }).collect::<Vec<String>>().join(" ");
+            match row_index {
+                0 if R == 1 => write!(f, "[ {} ]\n", row_string)?,
+                0 => write!(f, "┌ {} ┐\n", row_string)?,
+                int if int == R-1 => write!(f, "└ {} ┘\n", row_string)?,
+                _ => write!(f, "│ {} │\n", row_string)?
+            }
+        }
+
+        Ok(())
+    }
+}
+    
 impl<const R: usize, const C: usize> Mat<R,C> {
     fn zero() -> Self {
         Mat([[0.0; C]; R])
@@ -48,7 +69,7 @@ impl<const R: usize, const C: usize> Mat<R,C> {
 
 
 fn main() {
-    let m1 = Mat::<3,3>::identity();
-
-    dbg!(m1);
+    let mut m1 = Mat::<3,3>::identity();
+    m1.0[1][1] = 123.234;
+    println!("{}", m1);
 }
