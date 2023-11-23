e41fc7ca122f6f75b534d2fd6371568204c85e85
Alfio <tomarchioalfio5@gmail.com>
2023-11-23 11:06:57 +0000

    move to lib.rs, scalar division, WIP determinants

--- src/main.rs
+++ src/main.rs
@@ -1,96 +1,50 @@
-#![allow(dead_code)]
-use std::fmt::Display;
-use std::ops::{Deref, DerefMut};
-
-#[derive(Debug)]
-struct Mat<const R: usize, const C: usize>([[f64; C]; R]);
-
-impl<const R: usize, const C: usize> Deref for Mat<R, C> {
-    type Target = [[f64; C]; R];
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl<const R: usize, const C: usize> DerefMut for Mat<R, C> {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
-    }
-}
-
-impl<const R: usize, const C: usize> Display for Mat<R, C> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let max_column_lengths = self.transpose().map(|column| {
-            column
-                .map(|x| x.to_string().len())
-                .iter()
-                .copied()
-                .max()
-                .unwrap()
-        });
-
-        for (row_index, row) in self.iter().enumerate() {
-            let row_string = row
-                .iter()
-                .enumerate()
-                .map(|(column_index, column)| {
-                    format!(
-                        "{:^len$}",
-                        column.to_string(),
-                        len = max_column_lengths[column_index]
-                    )
-                })
-                .collect::<Vec<String>>()
-                .join(" ");
-            let (start_char, end_char) = match row_index {
-                0 if R == 1 => ("[", "]"),
-                0 => ("┌", "┐\n"),
-                int if int == R - 1 => ("└", "┘"),
-                _ => ("│", "│\n"),
-            };
-            write!(f, "{} {} {}", start_char, row_string, end_char)?;
-        }
-
-        Ok(())
-    }
-}
-
-impl<const R: usize, const C: usize, T: Into<f64>> From<[[T; C]; R]> for Mat<R, C> {
-    fn from(value: [[T; C]; R]) -> Self {
-        Self(value.map(|row| row.map(|n| n.into())))
-    }
-}
-
-impl<const R: usize, const C: usize> Mat<R, C> {
-    fn zero() -> Self {
-        Mat([[0.0; C]; R])
-    }
-
-    fn generate<F: Fn(usize, usize) -> f64>(f: F) -> Self {
-        let mut mat = Self::zero();
-
-        for (row_index, row) in mat.iter_mut().enumerate() {
-            for (column_index, n) in row.iter_mut().enumerate() {
-                *n = f(row_index, column_index);
-            }
-        }
-
-        mat
-    }
-
-    fn transpose(&self) -> Mat<C, R> {
-        Mat::<C, R>::generate(|row, column| self[column][row])
-    }
-}
-
-//functions exclusive to square matrices
-impl<const N: usize> Mat<N, N> {
-    fn identity() -> Self {
-        Self::generate(|row, column| if row == column { 1.0 } else { 0.0 })
-    }
-}
+use mat_rs::*;
 
 fn main() {
-    let m1: Mat<0, 0> = Mat::identity();
-    dbg!(m1);
+    let _33a = mat![
+        7, -4, 2;
+        3, 1, -5;
+        2, 2, -5
+    ]
+    .determinant();
+
+    let _33b = mat![
+       1, -6, -7;
+       1, -4,  7;
+      -1, -3, -6
+    ]
+    .determinant();
+
+    let _33c = mat![
+      -1, -1, -1;
+       4,  5, -3;
+      -1, -6,  3
+    ]
+    .determinant();
+
+    let _44a = mat![
+      4,  3,  2, 2;
+      0,  1, -3, 3;
+      0, -1,  3, 3;
+      0,  3,  1, 1
+    ]
+    .reduced_row_echelon_form();
+
+    let _44b = mat![
+       2,  3,  3, 1;
+       1,  5,  4, 3;
+       4,  6,  8, 5;
+      -2, -3, -3, 4
+    ]
+    .determinant(); //this is correct??????????????/
+
+    let _m1 = mat![
+          4, -4,   3, 2, 1;
+          0,  6,   2, 1, 7;
+          4,  3, -12, 4, 8;
+        -12,  3,   4, 4, 3;
+          1,  1, -12, 0, 1
+    ];
+
+    println!("{}", _44a.unwrap());
 }
